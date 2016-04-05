7e5ffec0c36a68b208d651a514a3949e6d8fe86d
artekatie <kate.prasolek@gmail.com>
2016-04-05 19:37:16 +0100

    New tech icons - bigger and all. Skills styled.

--- base.js
+++ base.js
@@ -1,12 +1,5 @@
-WebFontConfig = {
-    google: { families: [ 'Lora::latin' ] }
-};
-(function() {
-    var wf = document.createElement('script');
-    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-    wf.type = 'text/javascript';
-    wf.async = 'true';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(wf, s);
-})();
+
+$(document).ready(function(){
+  
+
+});
